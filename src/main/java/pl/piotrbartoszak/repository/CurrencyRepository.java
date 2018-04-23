f02d0ec0ba85ddd8a6da251eb9d44669417d09dd
Piotr <p_bartoszak@wp.pl>
2018-04-23 23:21:08 +0200

    Add repository to convert string to currency

--- src/main/java/pl/piotrbartoszak/repository/CurrencyRepository.java
+++ src/main/java/pl/piotrbartoszak/repository/CurrencyRepository.java
@@ -2,6 +2,7 @@ package pl.piotrbartoszak.repository;
 
 import org.springframework.stereotype.Repository;
 import pl.piotrbartoszak.model.Currency;
+import pl.piotrbartoszak.service.ReadFileService;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -19,7 +20,14 @@ public class CurrencyRepository {
 
     public CurrencyRepository() {
         this.currencies = new ArrayList<>();
-        
+        List<String> currenciesList = ReadFileService.readFile();
+
+        for(String s : currenciesList) {
+            Currency c = convertStringToCurrency(s);
+            if(c != null) {
+                currencies.add(c);
+            }
+        }
     }
 
     public List<Currency> getCurrencies() {
@@ -28,5 +36,29 @@ public class CurrencyRepository {
 
     public void setCurrencies(List<Currency> currencies) {
         this.currencies = currencies;
+    }
+
+    private Currency convertStringToCurrency(String s) {
+        String[] parts = s.split(",");
+        if(parts.length != 4) {
+            return null;
+        }
+
+        String countrySymbol = parts[0].trim();
+        String currencySymbol = parts[3].trim();
+
+        int taxPercent;
+        int fixedCosts;
+
+        try {
+            taxPercent = Integer.parseInt(parts[1].trim().replace("%",""));
+            fixedCosts = Integer.parseInt(parts[2].trim());
+        } catch (NumberFormatException e) {
+            return null;
+        }
+
+        Currency currency = new Currency(countrySymbol, taxPercent, fixedCosts, currencySymbol);
+
+        return currency;
     }
 }
