7e340555f726341a4bf97e56577a77e871f5ece9
Piotr <p_bartoszak@wp.pl>
2018-04-24 16:39:05 +0200

    Complete UI page

--- src/main/resources/view/javascript/app.js
+++ src/main/resources/view/javascript/app.js
@@ -9,6 +9,55 @@ $(document).ready(function () {
             url: API_URL
         }).done(function (countries) {
             console.log(countries);
+            showCountries(countries);
+        })
+    }
+
+    $('button').on('click', function (e) {
+        e.preventDefault();
+
+        var value = $('#value').val();
+
+        if($.isNumeric(value) & value > 0) {
+            $('#error').text('');
+
+            var valueToSend = {
+                value: value
+            };
+
+            var headers = new Headers({
+                'Content-Type': 'application/json'
+            });
+
+            var myInit = {
+                method: 'POST',
+                headers: headers,
+                cache: 'default',
+                body: JSON.stringify(valueToSend)
+            };
+
+            fetch(API_URL + '/calculate', myInit).then(function (response) {
+                showCountries(response);
+            })
+
+        } else {
+            $('#error').text('Błędna wartość');
+        }
+
+    })
+
+    function showCountries(countries) {
+        var ul = $('div.countries');
+        countries.forEach(function (country) {
+            var earning;
+            if(country.earning === undefined) {
+                earning = 'przelicz';
+            } else {
+                earning = country.earning;
+            }
+            var li = $('<p>Miesięczny zarobek w ' + country.countrySymbol +
+                ' - '+ earning +'</p>');
+            ul.append(li);
         })
     }
 })
